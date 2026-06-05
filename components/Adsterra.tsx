9f135c89894690fa476dcd1def51056b3d11b01c
Shobun Kollied Anwar <shobunkollieda@gmail.com>
2026-06-05 13:36:47 +0700

    add console log ads

--- components/Adsterra.tsx
+++ components/Adsterra.tsx
@@ -5,17 +5,20 @@ import { useEffect } from 'react';
 export default function Adsterra() {
     useEffect(() => {
         console.log('ADS SCRIPT LOADED');
+
         const script = document.createElement('script');
-        script.src =
-            'https://pl29644320.effectivecpmnetwork.com/68/d6/46/68d646480ec953570dd0c76a6f750526.js';
+        script.src = 'https://pl29644320.effectivecpmnetwork.com/68/d6/46/68d646480ec953570dd0c76a6f750526.js';
         script.async = true;
-        script.setAttribute('data-cfasync', 'false');
+
+        script.onload = () => {
+            console.log('ADS SCRIPT SUCCESS');
+        };
+
+        script.onerror = () => {
+            console.log('ADS SCRIPT ERROR');
+        };
 
         document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
     }, []);
 
     return null;
