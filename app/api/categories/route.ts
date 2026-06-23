1cbe58824a77d6653977d596bad99fcec11dc4ab
Shobun Kollied Anwar <shobunkollieda@gmail.com>
2026-06-24 06:38:21 +0700

    get category

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,14 +1,16 @@
-import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
+
 import { supabase } from '@/lib/supabase';
 
 export async function GET() {
-
-    const { data, error } = await supabase
+    const { data, error, count } = await supabase
         .from('categories')
-        .select('*');
+        .select('*', { count: 'exact' });
 
     return Response.json({
+        count,
         data,
         error,
+        supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
     });
 }
